8445cbb28105cdd3d10d822e447a97be350e503b
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-02-03 11:13:37 -0300

    Refatorando a App.js e MenuAcessibilidade.js para padrão React Hooks

--- src/App.js
+++ src/App.js
@@ -1,56 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import "./assets/css/styles.scss"
 import {Rotas} from "./componentes/Rotas";
 import {MenuPrincipal} from "./componentes/Menu/MenuPrincipal";
-import MenuAcessibilidade from "./componentes/Menu/MenuAcessibilidade";
+import {MenuAcessibilidade} from "./componentes/Menu/MenuAcessibilidade";
 import {Rodape} from "./componentes/Rodape/Rodape";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            alterarFonte:(localStorage.getItem("alterarFonte") && localStorage.getItem("alterarFonte") === "true") || false,
-            alterarContraste: (localStorage.getItem("alterarContraste") &&  localStorage.getItem("alterarContraste") === "true") || false,
+export const App = () =>{
+    const [alterarFonte, setAlterarFonte] = useState('');
+    const [alterarContraste, setAlterarConstraste] = useState('');
 
-        };
-        this.alterarFonte = this.alterarFonte.bind(this);
-        this.alterarContraste = this.alterarContraste.bind(this);
+    const handleFonte = () => {
+        setAlterarFonte(!alterarFonte)
     }
 
-    alterarFonte() {
-        const alterarFonte =
-            localStorage.getItem("alterarFonte") !== null
-                ? localStorage.getItem("alterarFonte") !== "true"
-                : true;
-        localStorage.setItem("alterarFonte", alterarFonte);
-        this.setState({ alterarFonte });
+    const handleConstraste = () => {
+        setAlterarConstraste(!alterarContraste)
     }
 
-    alterarContraste() {
-        const alterarContraste =
-            localStorage.getItem("alterarContraste") !== null
-                ? localStorage.getItem("alterarContraste") !== "true"
-                : true;
-        localStorage.setItem("alterarContraste", alterarContraste);
-        this.setState({ alterarContraste });
-    }
+    return(
+        <section role="main" className={`${alterarFonte && "fonte-maior"} ${alterarContraste && "alto-contraste"}`}>
+            <MenuAcessibilidade
+                handleFonte={handleFonte}
+                handleConstraste={handleConstraste}
+            />
+            <MenuPrincipal  />
+            <Rotas/>
+            <Rodape/>
+        </section>
+    )
 
-    render() {
-        const { alterarFonte, alterarContraste } = this.state;
-        return (
-            <section role="main" className={`${alterarFonte && "fonte-maior"} ${alterarContraste && "alto-contraste"}`}>
-                <MenuAcessibilidade
-                    alterarFonte={this.alterarFonte}
-                    alterarContraste={this.alterarContraste}
-                />
-                <MenuPrincipal  />
-                <Rotas/>
-                <Rodape/>
-            </section>
-
-        );
-    }
 }
-
-export default App;
