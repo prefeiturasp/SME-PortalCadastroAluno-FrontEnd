84d9150d97c2f217ee031e83f82101ac116514da
Marcelo Maia <mmaia.cc@gmail.com>
2020-02-21 10:02:56 -0300

    docker parametrizável.

--- src/services/ConectarApi.js
+++ src/services/ConectarApi.js
@@ -1,62 +1,66 @@
 const URL_API = process.env.REACT_APP_URL_API;
 const TOKEN = process.env.REACT_APP_TOKEN;
 
+if (process.env.NODE_ENV === "production") {
+  URL_API = "API_URL_REPLACE_ME";
+  TOKEN = "TOKEN_REPLACE_ME";
+}
+
 export async function buscarPalavrasImproprias() {
+  const requestInfo = {
+    method: "GET",
+    headers: {
+      Authorization: TOKEN,
+      "Content-type": "application/json",
+      Accept: "application/json"
+    }
+  };
 
-    const requestInfo = {
-        method: "GET",
-        headers: {
-            Authorization: TOKEN,
-            "Content-type": "application/json",
-            Accept: "application/json"
-        },
-    };
-
-    return await fetch(`${URL_API}/palavras-bloqueadas/`, requestInfo)
-        .then(resposta => {
-            return resposta.json();
-        });
+  return await fetch(`${URL_API}/palavras-bloqueadas/`, requestInfo).then(
+    resposta => {
+      return resposta.json();
+    }
+  );
 }
 
 export async function buscaDadosAlunoResponsavel(codigoEol, dtNascAluno) {
+  const requestInfo = {
+    method: "POST",
+    body: JSON.stringify({
+      codigo_eol: codigoEol,
+      data_nascimento: dtNascAluno
+    }),
+    headers: {
+      Authorization: TOKEN,
+      "Content-type": "application/json",
+      Accept: "application/json"
+    }
+  };
 
-    const requestInfo = {
-        method: "POST",
-        body: JSON.stringify({
-            codigo_eol: codigoEol,
-            data_nascimento: dtNascAluno,
-        }),
-        headers: {
-            Authorization: TOKEN,
-            "Content-type": "application/json",
-            Accept: "application/json"
-        },
-    };
-
-    return await fetch(`${URL_API}/dados-responsavel/busca_dados/`, requestInfo)
-        .then(resposta => {
-            return resposta.json();
-        });
+  return await fetch(
+    `${URL_API}/dados-responsavel/busca_dados/`,
+    requestInfo
+  ).then(resposta => {
+    return resposta.json();
+  });
 }
 
 export async function atualizaCadastro(dados) {
+  const requestInfo = {
+    method: "POST",
+    body: JSON.stringify(dados),
+    headers: {
+      Authorization: TOKEN,
+      "Content-type": "application/json",
+      Accept: "application/json"
+    }
+  };
 
-    const requestInfo = {
-        method: "POST",
-        body: JSON.stringify(dados),
-        headers: {
-            Authorization: TOKEN,
-            "Content-type": "application/json",
-            Accept: "application/json"
-        },
-    };
-
-    return await fetch(`${URL_API}/alunos/`, requestInfo)
-        .then(resposta => {
-            if (resposta.ok) {
-                return resposta.json();
-            } else {
-                throw new Error("Não foi possível obter os dados");
-            }
-        })
+  return await fetch(`${URL_API}/alunos/`, requestInfo).then(resposta => {
+    if (resposta.ok) {
+      return resposta.json();
+    } else {
+      throw new Error("Não foi possível obter os dados");
+    }
+  });
 }
