6fdee96fa5da2d121a60b17cfd60767add0bb14b
Calvin Rossinhole <calvin.masters@gmail.com>
2020-02-23 20:23:42 -0300

    fix 401 (usando token com um usuario sem permissao)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import "./assets/css/styles.scss"
-import {Rotas} from "./componentes/Rotas";
-import {MenuPrincipal} from "./componentes/Menu/MenuPrincipal";
-import {MenuAcessibilidade} from "./componentes/Menu/MenuAcessibilidade";
-import {Rodape} from "./componentes/Rodape/Rodape";
+import "./assets/css/styles.scss";
+import { Rotas } from "./componentes/Rotas";
+import { MenuPrincipal } from "./componentes/Menu/MenuPrincipal";
+import { MenuAcessibilidade } from "./componentes/Menu/MenuAcessibilidade";
+import { Rodape } from "./componentes/Rodape/Rodape";
 
-export const App = () =>{
-    const [alterarFonte, setAlterarFonte] = useState('');
-    const [alterarContraste, setAlterarConstraste] = useState('');
+export const App = () => {
+  const [alterarFonte, setAlterarFonte] = useState("");
+  const [alterarContraste, setAlterarConstraste] = useState("");
 
-    const handleFonte = () => {
-        setAlterarFonte(!alterarFonte)
-    }
+  const handleFonte = () => {
+    setAlterarFonte(!alterarFonte);
+  };
 
-    const handleConstraste = () => {
-        setAlterarConstraste(!alterarContraste)
-    }
+  const handleConstraste = () => {
+    setAlterarConstraste(!alterarContraste);
+  };
 
-    return(
-        <section role="main" className={`${alterarFonte && "fonte-maior"} ${alterarContraste && "alto-contraste"}`}>
-            <MenuAcessibilidade
-                handleFonte={handleFonte}
-                handleConstraste={handleConstraste}
-            />
-            <MenuPrincipal  />
-            <Rotas/>
-            <Rodape/>
-        </section>
-    )
-
-}
+  return (
+    <section
+      role="main"
+      className={`${alterarFonte && "fonte-maior"} ${alterarContraste &&
+        "alto-contraste"}`}
+    >
+      <MenuAcessibilidade
+        handleFonte={handleFonte}
+        handleConstraste={handleConstraste}
+      />
+      <MenuPrincipal />
+      <Rotas />
+      <Rodape />
+    </section>
+  );
+};
