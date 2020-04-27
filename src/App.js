1a5972c16e003eab3d491a6daf7a14e6b0a23664
Calvin Rossinhole <calvin.masters@gmail.com>
2020-04-27 17:33:37 -0300

    api version no rodape

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
-
+import { version } from "../package.json";
 import "./assets/css/styles.scss";
 import { Rotas } from "./componentes/Rotas";
 import { MenuPrincipal } from "./componentes/Menu/MenuPrincipal";
 import { MenuAcessibilidade } from "./componentes/Menu/MenuAcessibilidade";
 import { Rodape } from "./componentes/Rodape/Rodape";
 import { login, getToken } from "./services/auth.service";
+import { getAPIVersion } from "./services/ConectarApi";
 
 export const App = () => {
   const [alterarFonte, setAlterarFonte] = useState("");
   const [alterarContraste, setAlterarConstraste] = useState("");
+  const [apiVersion, setApiVersion] = useState(null);
 
   const handleFonte = () => {
     setAlterarFonte(!alterarFonte);
@@ -23,13 +25,17 @@ export const App = () => {
     if (!getToken()) {
       login();
     }
+    getAPIVersion().then((response) => {
+      setApiVersion(response.API_Version);
+    });
   });
 
   return (
     <section
       role="main"
-      className={`${alterarFonte && "fonte-maior"} ${alterarContraste &&
-        "alto-contraste"}`}
+      className={`${alterarFonte && "fonte-maior"} ${
+        alterarContraste && "alto-contraste"
+      }`}
     >
       <MenuAcessibilidade
         handleFonte={handleFonte}
@@ -37,7 +43,7 @@ export const App = () => {
       />
       <MenuPrincipal />
       <Rotas />
-      <Rodape />
+      <Rodape versao={`${version} (API: ${apiVersion})`} />
     </section>
   );
 };
